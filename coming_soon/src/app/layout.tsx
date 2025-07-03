c4192e394ee23a9f4ca45c7d15ba6c7c3f867d98
amCtrld <amctrld@gmail.com>
2025-07-04 00:09:19 +0300

    Configuring metadata

--- coming_soon/src/app/layout.tsx
+++ coming_soon/src/app/layout.tsx
@@ -1,40 +1,35 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "BowlRMS | Coming Soon",
   description: "A modern restaurant management system to revolutionize operations.",
   icons: {
     icon: "/favicon.ico",
   },
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <head>
-    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-  </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
-  );
+  )
 }
